6eb0659208b67d88d4eb8d812516ed6efd3c4885
RAM111111 <r.r.99@windowslive.com>
2021-12-06 10:18:17 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by R on 02/05/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
